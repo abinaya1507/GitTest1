dcd8541cd46706458c92b7dd602a5bf326762775
abinaya1507 <srithar.abinaya@gmail.com>
2018-10-25 17:55:26 +0530

    created a new method

--- src/org/git1/Sample1.java
+++ src/org/git1/Sample1.java
@@ -4,10 +4,15 @@ public class Sample1 {
 public static void main(String[] args) {
 	Sample1 s=new Sample1();
 	s.display();
+	s.print();
 	
 }
 public void display()
 {
 	System.out.println("Welcome to git hub");
+}
+public void print() {
+	// TODO Auto-generated method stub
+System.out.println("Adding another method");
 }
 }
