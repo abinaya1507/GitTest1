404d57881afd4d29fa2de5aaf14e7dd2d86e235e
abinaya1507 <srithar.abinaya@gmail.com>
2018-10-25 17:45:48 +0530

    Created a method and base class

---
+++ src/org/git1/Sample1.java
@@ -0,0 +1,13 @@
+package org.git1;
+
+public class Sample1 {
+public static void main(String[] args) {
+	Sample1 s=new Sample1();
+	s.display();
+	
+}
+public void display()
+{
+	System.out.println("Welcome to git hub");
+}
+}
